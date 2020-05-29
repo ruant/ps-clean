f832452fd99c3299a097ba857d4e2f814645f03b
Rune Antonsen <rune.antonsen@gmail.com>
2020-05-29 11:25:33 +0200

    fix: make the output look prettier, also added `packages` to filter

--- DeleteObjBinFolders.ps1
+++ DeleteObjBinFolders.ps1
@@ -1,51 +1,40 @@
 # PowerShell script that recursively deletes all 'bin' and 'obj' (or any other specified) folders inside current folder
 
 $CurrentPath = (Get-Location -PSProvider FileSystem).ProviderPath
+Write-Host "Please wait while searching for 'bin' and 'obj' folders to remove in $CurrentPath and it's subdirectories."
 
 # recursively get all folders matching given includes, except ignored folders
-$FoldersToRemove = Get-ChildItem .\ -include bin,obj -Recurse   | where {$_ -notmatch '_tools' -and $_ -notmatch '_build' -and $_ -notmatch 'node_modules'} | foreach {$_.fullname}
+$FoldersToRemove = @();
 
-# recursively get all folders matching given includes
-$AllFolders = Get-ChildItem .\ -include bin,obj -Recurse | foreach {$_.fullname}
+Write-Host "`rCurrently searching... So far we've found:"$FoldersToRemove.Count"to be removed." -NoNewline
 
-# subtract arrays to calculate ignored ones
-$IgnoredFolders = $AllFolders | where {$FoldersToRemove -notcontains $_} 
+Get-ChildItem .\ -include bin,obj -Recurse | Where-Object {$_ -notmatch '_tools' -and $_ -notmatch '_build' -and $_ -notmatch 'node_modules' -and $_ -notmatch 'packages'} | ForEach-Object {
+	Write-Host "`rCurrently searching... So far we've found:"$FoldersToRemove.Count"folders to be removed." -NoNewline
+	$FoldersToRemove += $_.fullname;
+}
+
+if($null -ne $FoldersToRemove)
+{
+	Write-Host "`rSearch completed, found:"$FoldersToRemove.Count"folders to be removed.                         " -foregroundcolor green
+}
+else {
+	Read-Host "`rNo folders to remove, press enter to exit." -foregroundcolor green
+	Exit
+}
 
 # remove folders and print to output
-if($FoldersToRemove -ne $null)
+if($null -ne $FoldersToRemove)
 {			
     Write-Host 
 	foreach ($item in $FoldersToRemove) 
 	{ 
-		remove-item $item -Force -Recurse;
+		Remove-Item $item -Force -Recurse;
 		Write-Host "Removed: ." -nonewline; 
 		Write-Host $item.replace($CurrentPath, ""); 
 	} 
 }
 
-# print ignored folders	to output
-if($IgnoredFolders -ne $null)
-{
-    Write-Host 
-	foreach ($item in $IgnoredFolders) 
-	{ 
-		Write-Host "Ignored: ." -nonewline; 
-		Write-Host $item.replace($CurrentPath, ""); 
-	} 
-	
-	Write-Host 
-	Write-Host $IgnoredFolders.count "folders ignored" -foregroundcolor yellow
-}
-
-# print summary of the operation
 Write-Host 
-if($FoldersToRemove -ne $null)
-{
-	Write-Host $FoldersToRemove.count "folders removed" -foregroundcolor green
-}
-else { 	Write-Host "No folders to remove" -foregroundcolor green }	
-
-Write-Host 
-
+Write-Host "Removed"$FoldersToRemove.Count"folders." -foregroundcolor green
 # prevent closing the window immediately
-$dummy = Read-Host "Completed, press enter to exit."
+Read-Host "Clean up completed, press enter to exit."
